9441473996cb66475303566b1f23b14e67bd44ec
martín <mvocilka20@alumnos.utalca.cl>
2023-04-30 20:47:58 -0400

    Versión final

--- Programas/Productor_Consumidor.c
+++ Programas/Productor_Consumidor.c
@@ -12,38 +12,38 @@ int contador;
 //2 segundos de espera para cada destino
 //CASA MATRIZ
 void productor(){
-    printf("Sucursal:\n");
     srand(time(NULL));
     while(itemCount != BUFFER_SIZE){
         buffer[contador] = rand()%(11-2+1);
         contador++;
         itemCount++;
     }
-    for (int i = 0; i < 20; i++){
-        printf("%d ", buffer[i]);
-    }
+
     contador = 0;
 }
 
 
 //REALIZAR LAS ENTREGAS
 void consumidor(){
+
+    for (int i = 0; i < 20; i++){
+        printf("%d ", buffer[i]);
+    }
+    printf("\n");
     sleep(2);
     ordenar();
     int cont = 0;
     int aux = buffer[19];
     for (int i = 0; i < itemCount; i++){
-        //sleep(1);
         cont++;
-        //printf("El camion llegò al destino: %d\n", buffer[i]+1);
         if (buffer[i] < buffer[i+1]){
             int a = (buffer[i+1] - buffer[i])*2;
-            printf("El camion llegò al destino: %d con %d paquetes\n", buffer[i]+1, cont);
+            printf("\nEl camion llegò al destino: %d con %d paquetes", buffer[i]+1, cont);
             cont = 0;
-            //sleep(a);
+            sleep(1);
         }
         if (buffer[i] == buffer[19] && i == itemCount-1){
-            printf("El camion llegò al destino: %d con %d paquetes\n", buffer[i]+1, cont);
+            printf("\nEl camion llegò al destino: %d con %d paquetes\n", buffer[i]+1, cont);
         }
     }
     //SE VACÌA EL BUFFER
@@ -51,7 +51,9 @@ void consumidor(){
         buffer[i] = -1;
     }
     int tiempoFinal = (aux+1)*2;
+    sleep(1);
     printf("\nEl camion se demorará %i segundos en llegar a la casa matriz desde el último destino\n", tiempoFinal);
+    printf("\n---------------------------------------------------------------------------------------\n");
 }
 
 //ORDENAR EL BUFFER
@@ -73,9 +75,8 @@ void ordenar(){
 int main(){
     productor();
     while (1){
-        sleep(4);
+        sleep(15);
         int pid = fork();
-        printf("\nid hijo: %d\n", pid);
         if (pid == 0){
             consumidor();
             break;
